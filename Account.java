1195aa2114ae9f0d166c15395b321f26d5865aee
jeremiah <tapej3124@wrdsb.ca>
2022-11-15 21:22:10 -0500

    Initial Commit

---
+++ Account.java
@@ -0,0 +1,31 @@
+public class Account {
+
+    private int accountNumber;
+    private String accountPassword;
+    private String accountName;
+    private Double funds;
+
+    public Account(int accountNumber, String accountPassword, String customerName,
+            Double funds) {
+
+    }
+
+    public int getAccountNumber() {
+    }
+
+    public String getAccountPassword() {
+    }
+
+    public String getAccountName() {
+    }
+
+    public Double getFunds() {
+    }
+
+    public Double addFunds(Double addedFunds) {
+    }
+
+    public String toString() {
+    }
+
+}
---
+++ AccountList.java
@@ -0,0 +1,39 @@
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AccountList {
+
+    private ArrayList<Account> accounts;
+    private String filename;
+
+    private Account updatedAcc;
+
+    public AccountList(String filename) {
+    }
+
+    private void readFile(String filename) {
+
+    }
+
+    public Account loginToAccount(int accountNumInput, String accountPassInput) {
+
+    }
+
+    public void depositFunds(Account a, Double addedFunds) {
+
+    }
+
+    public void withdrawFunds(Account a, Double removedFunds) {
+
+    }
+
+    public void viewBalance(Account a) {
+
+    }
+
+    private void writeFile(String filename) {
+      
+
+}
